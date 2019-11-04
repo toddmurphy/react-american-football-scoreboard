f6b27f654bc98bd98324d86650fb97021a362bc8
toddmurphy <toddmurphy93@gmail.com>
2019-11-04 01:16:54 -0400

    finished mvp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import './App.css';
 import BottomRow from './BottomRow';
 
+const homeTeam = 'Lions';
+const awayTeam = 'Tigers';
+const touchdown = 7;
+const fieldgoal = 3;
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0); //sets the initial home score state value to '0'
@@ -10,22 +15,22 @@ function App() {
 
   //onClick eventListener to increase homeScore by 7 points for touchdown
   const homeTouchdown = event => {
-    setHomeScore(homeScore + 7);
+    setHomeScore(homeScore + touchdown);
   };
 
   //onClick eventListener to increase homeScore by 3 points for fieldgoal
   const homeFieldgoal = event => {
-    setHomeScore(homeScore + 3);
+    setHomeScore(homeScore + fieldgoal);
   };
 
   //onClick eventListener to increase awayScore by 7 points for touchdown
   const awayTouchdown = event => {
-    setAwayScore(awayScore + 7);
+    setAwayScore(awayScore + touchdown);
   };
 
   //onClick eventListener to increase awayScore by 3 points for fieldgoal
   const awayFieldgoal = event => {
-    setAwayScore(awayScore + 3);
+    setAwayScore(awayScore + fieldgoal);
   };
 
   return (
@@ -33,7 +38,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -41,12 +46,13 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayTeam}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
       </section>
+
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
