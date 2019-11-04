6eb306e5dfda7b2b4d8b0033fb28ad26eef87539
toddmurphy <toddmurphy93@gmail.com>
2019-11-04 00:49:02 -0400

    add event listener onClick to make scores total

--- src/App.js
+++ src/App.js
@@ -37,12 +37,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -50,12 +50,20 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={homeTouchdown} className="homeButtons__touchdown">
+            Home Touchdown
+          </button>
+          <button onClick={homeFieldgoal} className="homeButtons__fieldGoal">
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={awayTouchdown} className="awayButtons__touchdown">
+            Away Touchdown
+          </button>
+          <button onClick={awayFieldgoal} className="awayButtons__fieldGoal">
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
